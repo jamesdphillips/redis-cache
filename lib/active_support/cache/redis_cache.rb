d3d216b14b5eb3fcbb58eeabeeb86ab902ec3cd2
James Phillips <jamesdphillips@gmail.com>
2012-10-21 23:13:10 -0700

    Redis cache.

---
+++ lib/active_support/cache/redis_cache.rb
@@ -0,0 +1,32 @@
+
+module ActiveSupport
+  module Cache
+    class RedisCache < Store
+
+      def initialize params
+        @client = Redis.new *params
+      end
+
+      protected
+
+      def write_entry key, entry, opts = {}
+        @client.send :set, key, entry
+      rescue Errno::ECONNREFUSED => e
+        false
+      end
+
+      def read_entry key, opts = {}
+        @client.send :get, key
+        entry.is_a?(ActiveSupport::Cache::Entry) ? entry : ActiveSupport::Cache::Entry.new(entry) if entry
+      rescue Errno::ECONNREFUSED => e
+        nil
+      end
+
+      def destroy_entry key, opts = {}
+        @client.send :del, key
+      rescue Errno::ECONNREFUSED => e
+        nil
+      end
+    end 
+  end
+end
